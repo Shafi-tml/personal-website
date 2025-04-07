e059730f6c90436ceef8d704c0f8204688264c0b
Iftekhar Ahmed Shafi <your.email@example.com>
2025-04-07 17:30:07 +0600

    Add Tailwind Typography plugin and update configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const withMDX = require('@next/mdx')()
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -18,4 +16,4 @@ const nextConfig = {
   output: 'standalone'
 }
 
-module.exports = withMDX(nextConfig) 
+module.exports = nextConfig 
